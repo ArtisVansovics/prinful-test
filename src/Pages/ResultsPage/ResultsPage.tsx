f25e4fd1b8c2efb0dfbf868e5036c80d02824509
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-19 14:58:44 +0300

    Results Page displays chosen username, the total number of questions in the current test, and how many of submitted answers were the same as correct answers, based on filter.

--- src/Pages/ResultsPage/ResultsPage.tsx
+++ src/Pages/ResultsPage/ResultsPage.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import { RootState } from '../../store';
+import { getTest } from '../../data/TestsData';
 
 const ResultsPage = () => {
+  const [correctAnswers, setCorrectAnswers] = useState<string[]>();
+  const [correctSubmittedAnswers, setCorrectSubmittedAnswers] = useState<string[]>();
   const userName = useSelector((state: RootState) => state.test.userName);
+  const submittedAnswers = useSelector((state: RootState) => state.test.answers);
+  const testId = useSelector((state: RootState) => state.test.testId);
+
+  useEffect(() => {
+    const test = getTest(testId);
+
+    if (test) {
+      setCorrectAnswers(test.correctAnswers);
+    }
+
+    if (correctAnswers) {
+      setCorrectSubmittedAnswers(submittedAnswers.filter((answer) => correctAnswers.includes(answer)));
+    }
+  }, [correctAnswers]);
+
+  const answerCount = correctAnswers?.length;
+  const correctCount = correctSubmittedAnswers?.length;
 
   return (
     <div className="page">
@@ -13,6 +33,9 @@ const ResultsPage = () => {
           <div className="col-xs-12">
             <div className="box">
               <PageTitle title={`Thanks, ${userName}`} />
+              <p>
+                {`You responded correctly to ${correctCount} out of ${answerCount} questions`}
+              </p>
             </div>
           </div>
         </div>
