de029ca11696aa510f1fbc3b0c03cc087d5ba61c
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-19 14:54:25 +0300

    Before user can move to next question, they need to choose an answer from TestPage, which is then saved to testSlice state

--- src/Pages/Test/TestPage.tsx
+++ src/Pages/Test/TestPage.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Question } from '../../models/TestModel';
-import { getQuestion } from '../../data/TestsData';
+import { useDispatch, useSelector } from 'react-redux';
+import { Question, Test } from '../../models/TestModel';
+import { AppDispatch, RootState } from '../../store';
+import { getQuestion, getTest } from '../../data/TestsData';
 import Button from '../../components/Button/Button';
 import PageTitle from '../../components/PageTitle/PageTitle';
+import { addAnswer } from '../../store/reducers/testReducer';
 
 const TestPage = () => {
   const [currentQuestion, setCurrentQuestion] = useState<Question>();
-  const { testId } = useParams();
+  const [currentTest, setCurrentTest] = useState<Test>();
+  const [currentAnswer, setCurrentAnswer] = useState('');
+  const testId = useSelector((state: RootState) => state.test.testId);
+  const answers = useSelector((state: RootState) => state.test.answers);
   const { questionId } = useParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    const question = getQuestion(Number(testId), Number(questionId));
+    const test = getTest(testId);
+    const question = getQuestion(testId, Number(questionId));
 
     if (question) {
+      setCurrentTest(test);
       setCurrentQuestion(question);
     } else navigate('/home');
   }, [testId, questionId]);
@@ -32,8 +41,38 @@ const TestPage = () => {
             <div className="col-xs-12">
               <div className="box">
                 {currentQuestion.answers.map((answer) => (
-                  <Button title={answer} />
+                  <Button
+                    title={answer}
+                    onClick={() => setCurrentAnswer(answer)}
+                  />
                 ))}
+              </div>
+            </div>
+            <div className="col-xs-12">
+              <div className="box">
+                {currentTest && answers.length < currentTest.questions.length - 1
+                  ? (
+                    <Button
+                      title="Next"
+                      disabled={currentAnswer === ''}
+                      onClick={() => {
+                        dispatch(addAnswer(currentAnswer));
+                        setCurrentAnswer('');
+                        navigate(`/test=${testId}/q=${Number(questionId) + 1}`);
+                      }}
+                    />
+                  )
+                  : (
+                    <Button
+                      title="See your results"
+                      disabled={currentAnswer === ''}
+                      onClick={() => {
+                        dispatch(addAnswer(currentAnswer));
+                        setCurrentAnswer('');
+                        navigate('/results');
+                      }}
+                    />
+                  )}
               </div>
             </div>
           </div>
